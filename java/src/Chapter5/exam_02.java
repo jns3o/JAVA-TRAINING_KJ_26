d601b0c0276596ab967168a904f1c6a82568a24d
jns3o <kj@DESKTOP-FDD8L2J>
2026-07-07 14:50:36 +0900

    참조,배열 첫 시간 클래스

---
+++ java/src/Chapter5/exam_02.java
@@ -0,0 +1,27 @@
+package Chapter5;
+
+public class exam_02 {
+
+	public static void main(String[] args) {
+		String str1 = ""; // 이건 아무것도 없는 메모리를 가리키고 있음
+		String str2 = null; // 어떤것도 가리키고 있지 않음
+		//int i = null; //없다 라는 의미는 값이 될 수 없음
+		System.out.println("str1 : " + str1);
+		System.out.println("str2 : " + str2);
+		
+		// ""/ null 비교
+		// 화장실에 갔는데 휴지심은 있고 휴지가 없는 상태 > ""
+		// 휴지심 조차도 없는 경우 > null
+		 
+		int [] num = {0,1,2,3};
+		
+		System.out.println(num);
+		
+		
+		
+		
+		
+		
+	}
+
+}
