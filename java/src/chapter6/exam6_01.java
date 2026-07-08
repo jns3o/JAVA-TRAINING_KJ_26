1b553ee0af05e97168c08112e37d43b43ec678ae
jns3o <shnsyp@gmail.com>
2026-07-08 15:57:47 +0900

    학교 7월8일

---
+++ java/src/chapter6/exam6_01.java
@@ -0,0 +1,21 @@
+package chapter6;
+
+
+
+public class exam6_01 {
+
+	public static void main(String[] args) {
+		Student std = new Student();
+		System.out.println(std.kor);
+		
+		std.kor = 3;
+		System.out.println(std.kor);
+	
+	
+	
+	
+	
+	}	
+	
+
+}
