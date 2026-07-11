c117e789e5f28631e313803b16903156471a6c2d
jns3o <shnsyp@gmail.com>
2026-07-11 15:48:30 +0900

    은행프로그램 2026-07-11 수정본

--- java/src/chapter6/BankProgram_homework.java
+++ java/src/chapter6/BankProgram_homework.java
@@ -54,6 +54,7 @@ public class BankProgram_homework {
 	}
 	static void createAccount(String[][] array) { // 계좌목록을 생성하기 위해 계좌번호와 예금주 명을 입력받는 메소드
 		Scanner sc = new Scanner(System.in);
+		while(true) {
 		System.out.println(">=========계좌 생성=========<");
 		System.out.println("계좌를 생성하기 위해 원하시는 계좌 번호 6자리와 예금주 명을 입력하시오(번호만 입력)");
 		System.out.print("\n-계좌번호 : ");
@@ -64,17 +65,18 @@ public class BankProgram_homework {
 			if (array[i][0] != null) {
 				if (array[i][0].equals(account)) {
 					overLap = true; 
+					break;
 			 	/*if문 안 array[i][0].equals(account)는 첫 계좌를 생성하고 한 바퀴를 돌았을때 위에서 사용자가 account에
 			 	원하는 계좌번호를 다시 입력하였는데 칸은 달라도 번호가 같아 버리면 (예: array[0][0] 하고 array[1][0]이 계좌번호
 			 	11자리가 모두 동일할 경우 위에서 boolean의 변수명이였던 overLap이 false에서 true가 되면서 중복되는 계좌번호이기에
  				다른 번호를 입력해주라는 문구를 출력*/						 
-					if (overLap == true) {
-						System.out.println("입력하신 계좌번호는 중복되는 번호입니다. 다른 번호를 입력해주세요\n");
-					}
-					 createAccount(array);// 중복이 발생하였을 경우 뒤로 되돌아가서 값을 재입력하기 위해 return;을 사용
-					 return;
+					
 				}
 			}
+		}
+		if (overLap == true) {
+			System.out.println("입력하신 계좌번호는 중복되는 번호입니다. 다른 번호를 입력해주세요\n");
+		 continue;
 		}
 		if (account.length() == 6) {
 			int num = -1; // 만약 info가[99][0~3]까지 다 차버렸을경우에 사용자가 값을 하나 더 입력해버리면 인덱스가 초과되어버리기에 초과되면
@@ -84,6 +86,9 @@ public class BankProgram_homework {
 					num = i;
 					break;
 				}
+			}if (num == -1) {
+				System.out.println("[!] 현재 Int.Public Bank의 온라인 계좌가 모두 가득찼습니다. 죄송합니다.\n");
+				continue;
 			}
 			System.out.print("-예금주 명 : ");
 			String name = sc.nextLine();
@@ -93,41 +98,42 @@ public class BankProgram_homework {
 			try {
 				int Firmoney = Integer.parseInt(money);
 				if (money.length() != String.valueOf(Firmoney).length()) {
-					System.out.println(" [!]금액의 첫 번째 자리에는 0이 올 수 없습니다\n" + " 다시 입력해주시기 바랍니다\n");
-					createAccount(array);
-					return;
+					System.out.println(" [!]금액의 첫 번째 자리에는 0이 올 수 없습니다\n" + " 다시 입력해주시기 바랍니다\n");			
+					continue;
 				}
 			} catch (Exception e) {
-				System.out.println(" 입금액은 숫자만 입력가능합니다\n" + " 다시 입력해주세요\n");
-				createAccount(array);
-				return;
+				System.out.println(" 입금액은 숫자만 입력가능합니다\n" + " 다시 입력해주세요\n");				
+				continue;
 			}
 			if (Integer.parseInt(money) == 0) {
-				System.out.println(" 초기 금액은 0원으로 할 수 없습니다\n" + " 다시 입력해주세요\n");
-				createAccount(array);
-				return;
+				System.out.println(" 초기 금액은 0원으로 할 수 없습니다\n" + " 다시 입력해주세요\n");				
+				continue;
 			}	
 			System.out.print(money + "원 입금확인되었습니다");
 			if (name.length() <= 6) {
 			System.out.println("\n>=========================<");
 			System.out.println();
-			}else {
-				System.out.println("[!]예금주 명은 6자까지 입력가능합니다, 다시 입력해주세요\n");
-				createAccount(array);
-				return;
-			}	
+			
+				
 			array[num][0] = account; // return을 쓰지 않고 info에 account와 name의 정보를 올리기 위해 createAccount메소드의 변환형은 없이
 			array[num][1] = name; // void로 만들고 array[num][0,1,2] = account / name / "0" ; 등으로 입력받게 해서
 			array[num][2] = money; // main 메소드에서 값을 바로 받아쓸 수 있게 하기 위해 입력
-
 			System.out.println("계좌번호: " + account + " | 예금주 명: " + name + " | 계좌 잔액: " + money + "\n" );
 			System.out.println("으로 저장되었습니다");
 			System.out.println();
-
+			break;
+			
+			}	else {
+				System.out.println("[!]예금주 명은 6자까지 입력가능합니다, 메인 메뉴로 돌아갑니다");
+				continue;
+			}
+	
+		
+			
 			} else {
-			System.out.println("입력된 계좌번호는 " + account.length() + " 자리입니다. 양식에 맞게 다시 입력해주세요\n");
-			createAccount(array);
-			return;
+			System.out.println("입력된 계좌번호는 " + account.length() + " 자리입니다. 양식에 맞게 다시 입력해주세요\n");			
+			continue;
+		}
 		}
 	}
 	static void AccList(String[][] array) { // AccList [AccountList] => 입력된 계좌 목록을 보여주기 위한 메소드
@@ -148,25 +154,64 @@ public class BankProgram_homework {
 	}
 	static void deposit(String[][] array) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println(">===========입금===========<");
+		while(true) {
+		boolean check = false;
+		System.out.println(">===========입금===========<\n");
+		System.out.print("입금하시고자 하는 계좌의 계좌번호를 입력해주세요 : ");
+		String account2 = sc.nextLine();
+		for (int i = 0; i < array.length; i++) {	
+			if(array[i][0] != null && array[i][0].equals(account2)) {
+					check = true;
+					System.out.println("-이 계좌가 당신의 계좌가 맞습니까?\n");
+					System.out.println("<계좌 번호> " + array[i][0] + "\t <예금주 명> " + array[i][1] + "\t <계좌 잔액> " +  array[i][2] + "\n");
+					System.out.print("이 계좌가 당신의 계좌가 맞다면 1, 아니라면 2를 입력해주세요 : ");					
+					String account3 = sc.nextLine();
+					System.out.println();
+					try {int temAccount = Integer.parseInt(account3);
+						if (temAccount == 1) {
+							System.out.println("확인되었습니다. 입금하고자 하는 금액을 입력해주세요");
+							System.out.println("[숫자로만 입력가능하며 입금액에 -(마이너스)는 들어갈 수 없고 입금액 앞에 0이 붙는 경우 시스템이 자동으로 빼고 계산합니다. 금액의 기준은 원화입니다](");
+							System.out.print("입금액 : ");
+							String pay = sc.nextLine();
+							try {int tempay = Integer.parseInt(pay);
+								if (tempay <= 0) {
+									System.out.println("입금액에 오류가 생겼습니다. 입금액에 -는 입력 불가합니다, 재입력 해주세요");
+									continue;									
+								}else {
+									array[i][2] = String.valueOf(Integer.parseInt(array[i][2]) + tempay);
+								}
+								
+								System.out.println("\n입금되었습니다\n" + "\n-현재 고객님의 계좌 내역입니다-\n<계좌번호>" + array[i][0] + " <예금자 명> " + array[i][1] + " <현재 계좌액> " + array[i][2] + "\n");						
+								return;
+							} catch (Exception e) {
+								System.out.println("입금액은 숫자로만 입력가능하며, 한글 또는 영어로 입력불가능 합니다. 재입력 하시오\n");
+								continue;
+							}
+						}else {
+							System.out.println("계좌를 다시 확인해주세요.\n");
+							continue;
+						}
+					} catch (Exception e) {
+						System.out.println("잘못입력하였습니다, 재입력 바랍니다\n");
+						continue;
+					}
+					
+		}
+		}if (check == false) {
+			System.out.println("현재 일치하는 계좌가 없습니다 \n");
+			System.out.println("처음으로 되돌아가시려면 1, 입금화면으로 돌아가시려면 2를 입력해주세요 : ");
+			String back = sc.nextLine();
+			if ("1".equals(back)) {
+				
+				return;
+			}if ("2".equals(back)) {
+				continue;
+			}
+		}
 		
 	}
 	
-	
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
+	}
 }
 
 
