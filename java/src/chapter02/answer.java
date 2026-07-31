18e6df0e20ed4eaa70de41a9a3a6f9fba0f72fd7
jns3o <shnsyp@gmail.com>
2026-07-31 16:35:40 +0900

    2026-07-31-4:35pm

--- java/src/chapter02/answer.java
+++ java/src/chapter02/answer.java
@@ -7,8 +7,8 @@ public class answer {
 		int age = 25;
 		String tel1= "010", tel2="123", tel3="4567";
 		System.out.println("이름: " + name);
-		System.out.print("나이: " + age);
-		System.out.printf("\n전화: " +  tel1 + "-" + tel2 + "-" + tel3);
+		System.out.print("나이: " + age + "\n");
+		System.out.printf("전화: %1$s-%2$s-%3$s", tel1, tel2, tel3);
 
 	}
 
