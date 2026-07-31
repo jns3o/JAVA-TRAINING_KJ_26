4d04ec96c49910b3b17c9d57bb9a796390fba690
jns3o <shnsyp@gmail.com>
2026-07-31 11:48:12 +0900

    2026-07-31

---
+++ java/src/chapter02/answer2.java
@@ -0,0 +1,22 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class answer2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫 번째 수: ");
+		String strNum1 = sc.nextLine();
+		
+		System.out.print("두 번째 수: ");
+		String strNum2 = sc.nextLine();
+		
+		int num1 = Integer.parseInt(strNum1);
+		int num2 = Integer.parseInt(strNum2);
+		int result = num1 + num2;
+		System.out.println("덧셈 결과 : " + result);
+
+	}
+
+}
